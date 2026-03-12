2377c2bd77e0c238f64a7efd55685fee419cf451
DVOanh <dvoanh25062004@gmail.com>
2026-03-12 11:09:36 +0700

    edit for uri

--- connect_mysql/connect.js
+++ connect_mysql/connect.js
@@ -1,13 +1,15 @@
 import mysql2 from 'mysql2/promise';
-const pool = mysql2.createPool(
-    // host: process.env.MYSQLHOST,
-    // user: process.env.MYSQLUSER,
-    // password: process.env.MYSQLPASSWORD,
-    // database: process.env.MYSQLDATABASE,
-    // port: process.env.MYSQLPORT,
+const pool = mysql2.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
 
-    process.env.MYSQL_URL
-);
+    waitForConnections: true,
+    connectionLimit: 10,
+    charset: 'utf8mb4'
+});
 // console.log('Ket noi bang pool');
 // TEST CONNECTION
 (async () => {
