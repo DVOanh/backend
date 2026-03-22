427406f80c750435d58e04fa50c66c981d85f656
DVOanh <dvoanh25062004@gmail.com>
2026-03-22 23:14:46 +0700

    them api danhmuc

--- api/danhmuc_api.js
+++ api/danhmuc_api.js
@@ -3,9 +3,14 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', async (req, res)=>{
-    const sql = "SELECT * from categories order by danhmuc_id desc";
-    const [rows] = await pool.query(sql);
-    return res.json(rows);
+    try{
+        const sql = "SELECT * from categories order by danhmuc_id desc";
+        const [rows] = await pool.query(sql);
+        return res.json(rows);
+    }
+    catch(error){
+        console.log("Loi server");
+    }
 });
 
 export default router;
