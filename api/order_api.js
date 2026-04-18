d9b39666f6bc2c8c7ec012d1584161c5e6f04676
DVOanh <dvoanh25062004@gmail.com>
2026-04-18 09:43:33 +0700

    Sua api donhang

--- api/order_api.js
+++ api/order_api.js
@@ -3,19 +3,29 @@ import express from 'express';
 const router = express.Router();
 import jwt from 'jsonwebtoken';
 
-router.get('/', async (req, res) => {
-    const { status_id } = req.query;
+router.get('/', async(req, res)=>{
+    const {status_id} = req.query;
     const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: 'Chua dang nhap' });
+    if(!authHeader){
+        return res.status(401).json({message: 'Chua dang nhap'});
     }
-    try {
+    try{
         const token = authHeader.split(" ")[1];
         const decoded = jwt.verify(token, "SECRE_KEY");
         const user_id = decoded.id;
         let sql = `
             SELECT 
-    o.*,
+    o.order_id,
+    o.user_id,
+    o.tongtien,
+    o.created_at,
+    o.hoten,
+    o.sdt,
+    o.diachi,
+    o.ghichu,
+    o.phuongthuc_thanhtoan,
+    o.order_code,
+    o.status_id,
     (
         SELECT p.image_url
         FROM order_items oi2
@@ -30,11 +40,11 @@ router.get('/', async (req, res) => {
 
 FROM orders o
 JOIN order_items oi ON o.order_id = oi.order_id
-WHERE o.user_id = ?
+WHERE o.user_id = 7
 GROUP BY o.order_id;
         `;
         let params = [user_id];
-        if (status_id) {
+        if(status_id){
             sql = sql + ' AND orders.status_id = ?';
             params.push(status_id);
         }
@@ -42,7 +52,7 @@ GROUP BY o.order_id;
         const [rows] = await pool.query(sql, params);
         return res.json(rows);
     }
-    catch (error) {
+    catch(error){
         console.log(error.message);
         return res.status(401).json({ message: "Token khong hop le" });
     }
@@ -58,7 +68,7 @@ router.post('/checkout', async (req, res) => {
             [user_id, total, hoten, phone, address, ghichu, pttt, orderCode]
         );
         const order_id = order.insertId;
-
+    
         await connection.query('INSERT INTO order_items(order_id, soluong_sp, price, variant_id) values (?, ?, ?, ?)',
             [order_id, quantity, price, variant_id]
         );
@@ -68,41 +78,41 @@ router.post('/checkout', async (req, res) => {
         );
 
         await connection.commit();
-        res.status(200).json({ message: 'Dat hang thanh cong' });
+        res.status(200).json({message: 'Dat hang thanh cong'});
     }
-    catch (error) {
+    catch(error){
         await connection.rollback();
         res.status(500).json({ message: "Lỗi đặt hàng", error: error.message });
     }
-    finally {
+    finally{
         connection.release();
     }
 });
 
-router.put('/huydon', async (req, res) => {
+router.put('/huydon', async (req, res)=>{
     const connection = await pool.getConnection();
     const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(400).json({ message: "Chua dang nhap" });
+    if(!authHeader){
+        return res.status(400).json({message: "Chua dang nhap"});
     }
-    try {
+    try{
         const token = authHeader.split(" ")[1];
         const decoded = jwt.verify(token, 'SECRE_KEY');
         const user_id = decoded.id;
         await connection.beginTransaction();
-        const { order_item_id, order_id, soluong_sp, variant_id } = req.body;
+        const {order_item_id, order_id, soluong_sp, variant_id} = req.body;
         const sqlhuydon = "UPDATE orders o join order_items oi ON o.order_id = oi.order_id set o.status_id = 10 WHERE user_id = ? and oi.order_id = ? and order_item_id = ?";
         const sqlsuaslkho = "UPDATE product_variant set stock = stock + ? WHERE id = ?"
         await connection.query(sqlhuydon, [user_id, order_id, order_item_id]);
         await connection.query(sqlsuaslkho, [soluong_sp, variant_id])
         await connection.commit();
-        res.status(200).json({ message: "Huỷ đơn thành công" });
+        res.status(200).json({message: "Huỷ đơn thành công"});
     }
-    catch (error) {
+    catch(error){
         await connection.rollback();
-        res.status(500).json({ message: "Lối hủy đơn" });
+        res.status(500).json({message: "Lối hủy đơn"});
     }
-    finally {
+    finally{
         connection.release();
     }
 })
