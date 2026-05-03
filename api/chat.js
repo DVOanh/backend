4172dc739f4a029cb6804966fda74fb5757bc49a
DVOanh <dvoanh25062004@gmail.com>
2026-05-03 22:37:40 +0700

    Sua api key

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import pool from "../connect_mysql/connect.js";
 
 const router = express.Router();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
