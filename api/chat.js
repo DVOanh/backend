2b988aa0a0d376d23554bb9998e8dedad0b25372
DVOanh <dvoanh25062004@gmail.com>
2026-05-03 22:33:31 +0700

    Sua api key

--- api/chat.js
+++ api/chat.js
@@ -1,9 +1,9 @@
 import express from "express";
-import {GoogleGenerativeAI} from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import pool from "../connect_mysql/connect.js";
 
 const router = express.Router();
-const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 router.post("/", async (req, res) => {
   try {
@@ -11,14 +11,14 @@ router.post("/", async (req, res) => {
     if (!message) {
       return res.status(400).json({ message: "Không có kí tự nào" });
     }
-    const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(message);
     const response = await result.response;
     const text = response.text();
-    res.json({reply: text});
+    res.json({ reply: text });
   } catch (error) {
-    console.error(error);
+    console.error("Gemini error:", error);
     res.status(500).json({ error: "Lỗi server" });
   }
 });
