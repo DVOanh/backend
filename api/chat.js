650664e0c6dc3871c521b7bdc565d7768de557c1
DVOanh <dvoanh25062004@gmail.com>
2026-05-09 08:20:22 +0700

    Them dssp cho chat bot

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Groq from "groq-sdk";
+import pool from "../connect_mysql/connect.js";
 
 const router = express.Router();
 const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
@@ -10,9 +11,39 @@ router.post("/", async (req, res) => {
     if (!message) {
       return res.status(400).json({ message: "Không có kí tự nào" });
     }
+    const sql = `
+      SELECT 
+  p.product_id,
+  p.product_name,
+  p.image_url,
+  v.id AS variant_id,
+  v.price,
+  s.total_sold
+FROM products p
+JOIN (
+    SELECT product_id, MIN(price) AS min_price
+    FROM product_variant
+    GROUP BY product_id
+) min_v ON min_v.product_id = p.product_id
+JOIN product_variant v 
+  ON v.product_id = p.product_id 
+  AND v.price = min_v.min_price
+JOIN (
+    SELECT product_id, SUM(sold) AS total_sold
+    FROM product_variant
+    GROUP BY product_id
+) s ON s.product_id = p.product_id ORDER BY product_id ASC LIMIT 3
+    `;
+    const [rows] = await pool.query(sql);
+    const productsText = rows.map(item=>
+      `${item.product_name} - ${item.price} VNĐ`
+    ).join("\n");
     const chatCompletion = await groq.chat.completions.create({
       messages: [
-        { role: "system", content: "Bạn là một trợ lý ảo tiếng Việt. Hãy trả lời thật ngắn gọn, súc tích, đi thẳng vào vấn đề. Tránh giải thích dài dòng trừ khi được yêu cầu." },
+        { role: "system", content: `Bạn là một trợ lý ảo tiếng Việt. 
+          Hãy trả lời thật ngắn gọn, súc tích, đi thẳng vào vấn đề. 
+          Tránh giải thích dài dòng trừ khi được yêu cầu.
+          Danh sách sản phẩm ${productsText}` },
         { role: "user", content: message }
       ],
       // Model Llama 3.3 này rất thông minh và hiểu tiếng Việt cực tốt
